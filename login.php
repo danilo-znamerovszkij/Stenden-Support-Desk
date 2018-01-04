375732447543f6ed987f22fe0423854f733c72d4
ErikNaljota <31888894+ErikNaljota@users.noreply.github.com>
2018-01-05 00:57:04 +0200

    Added login.php and loginCSS.css
    Additionally made mainCSS look more consistent.

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php //put your php code here
+	  //please use $result to display error messages, the html part is adapted to print out errors (currently lines 35-46 have to code on them)
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Stenden eHelp Login</title>
+		<link rel="stylesheet" type="text/css" href="css/mainCSS.css">
+		<link rel="stylesheet" type="text/css" href="css/loginCSS.css">
+	</head>
+
+	<body>
+
+	<div id="inputs_login">
+
+		<div class="logo">
+			<img src="img/logo.png" alt="Stenden eHelp Logo">
+		</div>
+		<div class="fields">
+			<form action="#" method="post" name="login">
+					<p><div class="fieldsText">Username </div><input class="fieldsInput" style="width: 100%;" type="text" name="username" width="50px"></p>
+					<p><div class="fieldsText">Password </div><input class="fieldsInput" style="width: 100%;" type="Password" name="password"></p>
+					<p><input class="fieldsText button" style=" margin-top: 10px; float: left;" type="submit" name="submit" value="Login"></p>
+			</form>
+
+			<form action="index.php">
+				<input class="fieldsText button" style="float: right; margin-top: 10px;" type="submit" name="submit" value="Home">
+			</form>
+		</div>
+
+		<?php
+		if (isset($_POST['submit'])){
+
+			echo '
+			<div class="response">
+
+				<p style="margin-left: 10px;">'; echo $result; echo' </p>
+			
+			
+			</div>';
+		}
+		?>
+
+	</div>
+
+	</body>
+</html>
