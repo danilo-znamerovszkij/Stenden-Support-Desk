b06e089b5880eba0c230c61545d5117e34adebbc
ErikNaljota <31888894+ErikNaljota@users.noreply.github.com>
2018-01-10 17:10:18 +0100

    logout button on index

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<?php session_start()
+<?php session_start();
 if (isset($_SESSION['o_username'])) {
   echo $_SESSION['o_username'];
+}elseif(isset($_SESSION['u_username'])){
+  echo $_SESSION['u_username'];
 }
 ?>
 
@@ -11,6 +13,7 @@ if (isset($_SESSION['o_username'])) {
     </head>
     <body>
         <a href="login.php">Login page</a>
+        <a href="logout.php">Logout</a>
 
     </body>
 </html>
