06df27045504394edab2aaf2de9de20fc1747811
ErikNaljota <31888894+ErikNaljota@users.noreply.github.com>
2018-01-12 13:28:57 +0100

    worked on faq.php

--- faq.php
+++ faq.php
@@ -78,14 +78,59 @@ if (isset($_SESSION['u_username'])) {
               </a>
 
             </div>
+
+            <div class="content">
+                <div class="faq">
+
+                  <article>
+                      <h2>Q: What do I do when I can’t see my database?</h2>
+                      <p>A: Log out of the system and then log back in. If the problem persists please send a ticket to us</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: There is no display after I log in?</h2>
+                      <p>A: Make sure you monitor is connected to a power source. After refresh the page. If nothing changes log out of the system and log back in. If the problem persists reinstall the system</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: What if my problem is not in the available tickets?</h2>
+                      <p>A:  (Only applies to those with a maintenance license): If you scroll to the bottom of the page you will see a section where you can add a ticket yourself with your own descriptions</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: What do I do if my ticket does not submit?</h2>
+                      <p>A: (Only applies to those with a maintenance license): Refresh the page, if the problem persists then log out and log back into the system</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: What do I do if my backup is lost?</h2>
+                      <p>A: Send us a ticket and we will re upload your backup into the system</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: I am unable to see the picture of the person working on my ticket</h2>
+                      <p>A: (Only applies to those with a maintenance license): Refresh the page, if nothing changes log out of the system and log back in. If the problem persists send us a ticket</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: (Only applies to those with a maintenance license) No one has taken my ticket</h2>
+                      <p>A: If this occurs it means our staff is overwhelmed please be patient and we will get to your ticket as soon as possible</p>
+                  </article>
+
+                  <article>
+                      <h2>Q: What if my details are incorrect</h2>
+                      <p>A: Send us a ticket within the correct information and we will update your details</p>
+                  </article>
+
+                </div>
+            </div>
+
             <div class="footer">
 
 
                 <div class="terms">
                     <p>Terms and conditions</p>
                 </div>
-
-
 
                 <div class="copyright">
 
