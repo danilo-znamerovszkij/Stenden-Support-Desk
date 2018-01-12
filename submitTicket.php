5baff5a25fb0f00b4541d159534f3beff73b9b0b
Ioana - Andreea Cojocaru <31888257+CIA1998@users.noreply.github.com>
2018-01-12 16:31:43 +0100

    submitTicket layout done

--- submitTicket.php
+++ submitTicket.php
@@ -15,6 +15,7 @@ if ($_SESSION['u_license'] == 0) {
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css/mainCSS.css">
+        <link rel="stylesheet" href="css/submitCSS.css">
         <title>Submit Ticket</title>
     </head>
     <body>
@@ -43,7 +44,44 @@ if ($_SESSION['u_license'] == 0) {
             <div class="titleDivider">
 
             </div>
+            <div class="content">
+                <div class="content1">
+                    <div id="softwareName">Software Name
+                        <div class="line1"></div>
+                         <div class="mainselection">
+                            <select name="software" id="input7">
+                                <option>Financial software</option>
+    <!--                            <option value="Alabama">Alabama</option>
 
+                                <option value="Wisconsin">Wisconsin</option>
+                                <option value="Wyoming">Wyoming</option>    -->                          
+                            </select>
+                            </div> 
+                    </div>
+                    <div id="problemType"> Problem Type
+                        <div class="line2"></div>
+                            <div class="mainselection">
+                                <select name="problem" id="input7">
+                                    <option>Choose the type</option>
+                                    <option value="Alabama">Crash</option>
+
+                                    <option value="Wisconsin">Crash</option>
+                                    <option value="Wyoming">Crash</option>                              
+                                </select>
+                            </div> 
+                           
+                    </div>
+                    <input type="submit" id="submit">
+                </div>
+                
+                
+                <div class="content2">
+                    <div id="pbd">Problem Description
+                        <div class="line3"></div>
+                        <textarea></textarea>
+                    </div>
+                </div>
+            </div>
             <div class="navWrapper">
 
               <a href="faq.php">
