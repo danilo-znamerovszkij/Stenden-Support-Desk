a27e0f07f576b54ad3aa9c568bdd5098d0837272
Ioana - Andreea Cojocaru <31888257+CIA1998@users.noreply.github.com>
2018-01-22 15:34:35 +0100

    footer

--- tickets.php
+++ tickets.php
@@ -112,7 +112,7 @@
                                 <td><?= $row['start_date'] ?></td>
                                 <td><?= $row['category_name'] ?></td>
                                 <td><?= $row['status_name'] ?></td>
-                                <td><a href='ViewTicket.php?ticket=<?= $row['incident_id'] ?>'><img src='img/logo.png' alt='logo link' width='25px' height='25px'></a></td>
+                                <td><a href='ViewTicket.php?ticket=<?= $row['incident_id'] ?>'><img src='img/logo.png' alt='logo link' width='25' height='25'></a></td>
                                 <td><form action="tickets.php" method="post">
                                     <input type="hidden" name="id" value="<?= $row['incident_id'] ?>">
 
@@ -137,7 +137,7 @@
                                       echo "<input style='margin-left:10px;' type='submit' value='Assign' name='submit'>";
 
                                     }else{//operator view
-                                      ?><input type="submit" value="Assign to me" name="submit">
+                                        ?> <input type="submit" value="Assign to me" name="submit">
                                       <!-- <input type="submit" value="Assign to me" name="submit"> -->
                                     <?php } ?>
 
@@ -183,17 +183,20 @@
                                         <td><a href='ViewTicket.php?ticket=<?= $row1['incident_id'] ?>'><img src='img/logo.png' alt='logo link' width='25px' height='25px'></a></td>
                                         <td> <?= $opName['name'] ?></td>
                                     </tr>
+                            </table>
                                 <?php } ?>
                               <?php } ?>
+        </div>
+            
 
-                          </div>
-                      </div>
-                      <div class="footer">
-                          <div class="terms">
-                              <p>Terms and conditions</p>
-                          </div>
-                          <div class="copyright"></div>
-                      </div>
-                  </div>
+                                    
+            </div>
+            <div class="footer">
+                <div class="terms">
+                    <p>Terms and conditions</p>
+                </div>
+                <div class="copyright"></div>
+            </div>
+        
               </body>
           </html>
