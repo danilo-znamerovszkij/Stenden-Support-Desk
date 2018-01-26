7a5a781e76b3aaf792f693baab5190b16162a8f6
ErikNaljota <31888894+ErikNaljota@users.noreply.github.com>
2018-01-26 15:54:35 +0100

    signup page

---
+++ signup.php
@@ -0,0 +1,24 @@
+<?php
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+</head>
+<body>
+  <form method="post" action="#">
+    <input type="text" name="employee" placeholder="isEmployee">
+    <input type="text" name="name" placeholder="username">
+    <input type="text" name="phone" placeholder="phone">
+    <input type="text" name="email" placeholder="email">
+    <input type="text" name="username" placeholder="username">
+    <input type="text" name="password" placeholder="password">
+    <input type="text" name="has_license" placeholder="has license">
+    <input type="text" name="position" placeholder="position">
+    <input type="text" name="contactId" placeholder="contactID">
+  </form>
+</body>
+</html>
